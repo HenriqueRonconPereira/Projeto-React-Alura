cc14650ec370f90099b1b4ace157fd94ad1b1c37
HenriqueRonconPereira <henriqueroncon@hotmail.com>
2024-02-27 20:28:11 -0300

    criação página favoritos

---
+++ src/pages/Favoritos/index.js
@@ -0,0 +1,23 @@
+import styles from './Favoritos.module.css';
+import videos from 'json/db.json';
+import Banner from 'components/Banner';
+import Titulo from 'components/Titulo';
+import Card from 'components/Card';
+
+function Favoritos(){
+    return(
+        <>
+            <Banner imagem="favoritos"/>
+            <Titulo>
+                <h1>Meus Favoritos</h1>
+            </Titulo>
+            <section className={styles.container}>
+                {videos.map((video) => {
+                    return <Card {...video} key={video.id} />
+                })}
+            </section>
+        </>
+    )
+}
+
+export default Favoritos;
