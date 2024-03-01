fc8ed1655b9a587d9b2d31d502d0faac5446b1cf
HenriqueRonconPereira <henriqueroncon@hotmail.com>
2024-03-01 16:31:14 -0300

    finalização do projeto

--- src/pages/Player/index.js
+++ src/pages/Player/index.js
@@ -1,15 +1,21 @@
 import Banner from 'components/Banner';
 import styles from './Player.module.css';
-import videos from 'json/db.json';
 import Titulo from 'components/Titulo';
 import { useParams } from 'react-router-dom';
 import NaoEncontrada from 'pages/NaoEncontrada';
+import { useEffect, useState } from 'react';
 
 function Player(){
+    const [video, setVideo] = useState();
     const parametros = useParams();
-const video = videos.find((video) => {
-        return video.id === Number(parametros.id);
-    })
+    
+    useEffect(() => {
+        fetch(`https://my-json-server.typicode.com/HenriqueRonconPereira/cinetag-api/videos?id=${parametros.id}`)
+        .then(resposta => resposta.json())
+        .then(dados => {
+            setVideo(...dados)
+        })
+    }, [])
 
     if(!video) {
         return <NaoEncontrada />
